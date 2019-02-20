f5bcc35cc222fe5f412214dcf8304fd127545345
sonkadak <45662574+sonkadak@users.noreply.github.com>
2019-02-20 16:30:25 +0900

    add time-stamp

--- port-check.py
+++ port-check.py
@@ -1,7 +1,7 @@
 # check the remote host's port until it open and send message
 
 import socket
-import time
+import time, datetime
 import requests
 
 ipaddr = 'IPADDRESS'
@@ -10,6 +10,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 url = '''
 https://api.telegram.org/bot[BOT_TOKEN]/sendmessage?chat_id=[CHAT_ID]&text='''+ipaddr+''':'''+str(port)+''' opened !
 '''
+print (datetime.datetime.now())
 while True:
     result = sock.connect_ex((ipaddr,port))
     if result == 0:
