6cd8903cc71e8583f7a086b53f63a21e573c8898
sonkadak <45662574+sonkadak@users.noreply.github.com>
2019-03-11 14:01:34 +0900

    Additional argument for session condition

--- sess_chk.sh
+++ sess_chk.sh
@@ -5,13 +5,19 @@ msg=$ipaddr' 현재 활성 세션 수: '$chk
 bot_token='MY_TOKEN'
 chat_id=CHAT_ID
 curl -s -o /dev/null "https://api.telegram.org/bot$bot_token/sendmessage?chat_id=$chat_id&text=$msg"
+if [ -z "$1" ]
+then
+        max=10
+else
+        max=$1
+fi
 
 while true
 do
         chk=$(netstat -ant |grep EST |wc -l)
-        if [ $chk -lt 10 ]
+        if [ $chk -lt $max ]
         then
-                msg=$ipaddr'작업 가능!  현재 활성 세션 수: '$chk
+                msg=$ipaddr'작업 가능! '$max' 미만 현재 활성 세션 수: '$chk
                 curl -s -o /dev/null "https://api.telegram.org/bot$bot_token/sendmessage?chat_id=$chat_id&text=$msg"
                 break
         fi
